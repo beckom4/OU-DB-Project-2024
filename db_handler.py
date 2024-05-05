dd3731458553594ecaee20805877421d21a5282b
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-05-05 21:57:12 +0300

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -1,60 +1,84 @@
-import oracledb
+import psycopg2
 
 
 class DB_handler:
     def __init__(self):
         # Connect to the database. PLEASE MAKE SURE TO change the  credentials to the ones on your local server.
-        self.connection = oracledb.connect(user="system", password="oracle", dsn="192.168.1.112:1521/XEPDB1")
+        self.connection = psycopg2.connect(dbname="db_project", user="omri", password="omri",
+                                           options="-c search_path=m_text")
         self.cursor = self.connection.cursor()
+
+    #
+
+    def create_types(self):
+        self.cursor.execute(" create type position_type as ( line_number INTEGER, position_in_line INTEGER); ")
+        self.connection.commit()
+        self.cursor.execute(" CREATE TYPE occurence_type AS ( magazine_id UUID, volume_id INTEGER, "
+                                                            " article_id INTEGER, positions_array position_type[]); ")
+        self.connection.commit()
 
     # Function to create all the required tables:
     # Magazines, Volumes, Articles, Authors,
     # As part of the creation of the articles table, a new type is created to store the article's pages
     def create_tables(self):
-        self.cursor.execute(" CREATE TABLE Magazines(magazine_id VARCHAR2(36) DEFAULT RAWTOHEX(SYS_GUID()) PRIMARY KEY,"
-                            " magazine_name VARCHAR(50),e_date DATE, location VARCHAR(50))")
-        self.cursor.execute(" CREATE TABLE Volumes(magazine_id VARCHAR2(36), volume_id INTEGER, issue_date DATE,"
-                            " CONSTRAINT pk_volumes PRIMARY KEY (magazine_id, volume_id),"
-                            " CONSTRAINT magazine_id_fk FOREIGN KEY (magazine_id) REFERENCES Magazines (magazine_id))")
-        self.cursor.execute(" CREATE OR REPLACE TYPE page_range_type AS TABLE OF INTEGER")
-        self.cursor.execute(" CREATE TABLE Articles( magazine_id VARCHAR(36), volume_id NUMBER, article_id NUMBER," 
-                            " article_title VARCHAR(100), page_range page_range_type,author_id NUMBER, "
-					        " CONSTRAINT articles_pk PRIMARY KEY (magazine_id, volume_id, article_id), "
-					        " CONSTRAINT articles_fk FOREIGN KEY (magazine_id, volume_id) "
-                            " REFERENCES Volumes (magazine_id, volume_id) "
-					        " )NESTED TABLE page_range STORE AS article_page_ranges")
+        self.cursor.execute(" CREATE TABLE m_text.Magazines(magazine_id UUID PRIMARY KEY, "
+                            " magazine_name TEXT, e_date DATE, location TEXT); ")
+        self.connection.commit()
+        self.cursor.execute(" CREATE TABLE m_text.Volumes(magazine_id UUID, volume_id INT, issue_date DATE, "
+					        " CONSTRAINT pk_volumes PRIMARY KEY (magazine_id, volume_id), "
+					        " CONSTRAINT magazine_id_fk FOREIGN KEY (magazine_id) "
+                            " REFERENCES m_text.Magazines (magazine_id)) ")
+        self.connection.commit()
+        self.cursor.execute(" CREATE TABLE m_text.Articles( magazine_id UUID, volume_id INT, article_id INT, "
+                            " article_title TEXT, page_range INT[] ,author_id INT, "
+                            " CONSTRAINT articles_pk PRIMARY KEY (magazine_id, volume_id, article_id), "
+                            " CONSTRAINT articles_fk FOREIGN KEY (magazine_id, volume_id) "
+                            " REFERENCES m_text.Volumes (magazine_id, volume_id)) ")
+        self.connection.commit()
         # Create a sequence to automatically generate a serial author_id
-        self.cursor.execute(" CREATE SEQUENCE author_id_seq START WITH 1 INCREMENT BY 1 NOMAXVALUE ")
-        self.cursor.execute(" CREATE TABLE authors(author_id INTEGER, first_name VARCHAR2(30), last_name VARCHAR(30), "
-                            " date_of_birth DATE, country VARCHAR2(30), "
-					        " CONSTRAINT authors_pk PRIMARY KEY (author_id)) ")
+        self.cursor.execute(" CREATE TABLE m_text.authors(author_id SERIAL PRIMARY KEY, first_name TEXT, last_name TEXT, "
+                            " date_of_birth DATE, country TEXT) ")
+        self.connection.commit()
+        self.cursor.execute(" CREATE TABLE m_text.words ( word_id SERIAL PRIMARY KEY, "
+                                                        " word TEXT, occurence occurence_type); ")
+        self.connection.commit()
+        self.cursor.execute(" CREATE TABLE user_input.special_words(group_id SERIAL PRIMARY KEY, "
+                            " group_description TEXT, words TEXT[])")
+        self.connection.commit()
+
     def create_triggers(self):
         # Create a trigger that will automatically increment the volume_id when a new volume is added.
-        self.cursor.execute(" CREATE OR REPLACE TRIGGER generate_volume_id" 
-                            " BEFORE INSERT ON Volumes"  
-                            " FOR EACH ROW"
-                            " DECLARE"
-                            " v_count INTEGER;"
-                            " BEGIN"
-                                " SELECT COUNT(*) INTO v_count FROM Volumes WHERE magazine_id = :NEW.magazine_id;"
-                                " :NEW.volume_id := v_count + 1;"
-                            " END;")
-        # Create a trigger that will automatically increment the article_id when a new article is added.
-        self.cursor.execute(" CREATE OR REPLACE TRIGGER generate_article_id "
-                            " BEFORE INSERT ON Articles "
-                            " FOR EACH ROW "
+        self.cursor.execute(" CREATE OR REPLACE FUNCTION m_text.generate_volume_id() "
+                            " RETURNS TRIGGER AS $$ "
                             " DECLARE "
-                                " a_count INTEGER; "
+                                " v_count INTEGER; "
                             " BEGIN "
-	                            " SELECT COUNT(*) INTO a_count " 
-                                " FROM Articles " 
-                                " WHERE magazine_id = :NEW.magazine_id AND volume_id = :NEW.volume_id; "
-                                " :NEW.article_id := a_count + 1; "
-                            " END;")
-        # create a trigger that will automatically generate a new serial author_id
-        self.cursor.execute(" CREATE OR REPLACE TRIGGER generate_author_id "
-                            " BEFORE INSERT ON authors "
+                                " SELECT COUNT(*) INTO v_count FROM Volumes WHERE magazine_id = NEW.magazine_id; "
+                                " NEW.volume_id := v_count + 1; "
+                                " RETURN NEW; "
+                            " END; "
+                            " $$ LANGUAGE plpgsql; "
+                            " CREATE TRIGGER trigger_generate_volume_id "
+                            " BEFORE INSERT ON m_text.Volumes "
                             " FOR EACH ROW "
+                            " EXECUTE FUNCTION generate_volume_id();")
+        self.connection.commit()
+        # Create a trigger that will automatically increment the article_id when a new article is added.
+        self.cursor.execute(" CREATE OR REPLACE FUNCTION m_text.generate_article_id() "
+                            " RETURNS TRIGGER AS $$ "
+                            " DECLARE "
+                                " a_count INTEGER;" 
                             " BEGIN "
-                                " SELECT author_id_seq.NEXTVAL INTO :NEW.author_id FROM dual; "
-                            " END;")
+                                " SELECT COUNT(*) INTO a_count " 
+                                " FROM Articles " 
+                                " WHERE magazine_id = NEW.magazine_id AND volume_id = NEW.volume_id; "
+                                " NEW.article_id := a_count + 1; "
+                                " RETURN NEW; "
+                            " END; "
+                            " $$ LANGUAGE plpgsql; "
+                            " CREATE TRIGGER trigger_generate_article_id "
+                            " BEFORE INSERT ON m_text.Articles "
+                            " FOR EACH ROW "
+                            " EXECUTE FUNCTION generate_article_id();")
+        self.connection.commit()
+
