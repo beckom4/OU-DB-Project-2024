5af63c651184e21623ff27f92323ea5815d82235
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-06-07 15:19:52 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,19 @@
+
 from datetime import datetime
 
 import psycopg2
 
 from DB_handler import DB_handler
 
+from SearchWizard import SearchWizard
+
 # Connect to the database. PLEASE MAKE SURE TO change the  credentials to the ones on your local server.
-connection = psycopg2.connect(dbname="db_project", user="omri", password="omri", options="-c search_path=m_text")
+connection = psycopg2.connect(dbname="db_project", user="omri", password="omri", options="-c search_path=text_handle")
 cursor = connection.cursor()
 
 db_test = DB_handler()
+
+db_test.create_schemas()
 
 db_test.create_types()
 
@@ -16,20 +21,22 @@ db_test.create_tables()
 
 db_test.create_triggers()
 
+sw = SearchWizard()
+
 ## Tests on Magazines table
 ##
 # cursor.execute("CREATE TABLE Magazines(magazine_id VARCHAR2(36) DEFAULT RAWTOHEX(SYS_GUID()) PRIMARY KEY,"
 #                            "magazine_name VARCHAR(50),e_date DATE, location VARCHAR(50))")
-cursor.execute(" INSERT INTO m_text.Magazines (magazine_id, magazine_name, e_date, location) VALUES (gen_random_uuid (), "
+cursor.execute(" INSERT INTO art_info.Magazines (magazine_id, magazine_name, e_date, location) VALUES (gen_random_uuid (), "
                " 'National Geographic', '1923-05-30', 'New York')")
-cursor.execute(" INSERT INTO m_text.Magazines (magazine_id, magazine_name, e_date, location) VALUES (gen_random_uuid (), "
+cursor.execute(" INSERT INTO art_info.Magazines (magazine_id, magazine_name, e_date, location) VALUES (gen_random_uuid (), "
                " 'TIME', '1987-05-30', 'Chicago') ")
 print("Magazines table: ")
-cursor.execute("SELECT * FROM magazines")
+cursor.execute("SELECT * FROM art_info.magazines")
 for row in cursor:
     print(row)
 
-cursor.execute("SELECT magazine_id FROM m_text.magazines WHERE magazine_name = 'National Geographic'")
+cursor.execute("SELECT magazine_id FROM art_info.magazines WHERE magazine_name = 'National Geographic'")
 magazine_id = cursor.fetchall()[0][0]
 
 # cursor.execute("TRUNCATE TABLE magazines")
@@ -44,14 +51,14 @@ magazine_id = cursor.fetchall()[0][0]
 #                     "CONSTRAINT magazine_id_fk FOREIGN KEY (magazine_id) REFERENCES Magazines (magazine_id)")
 cursor.close()
 cursor = connection.cursor()
-cursor.execute("INSERT INTO m_text.Volumes (magazine_id, issue_date) VALUES (%s, %s)",
+cursor.execute("INSERT INTO art_info.Volumes (magazine_id, issue_date) VALUES (%s, %s)",
                (magazine_id, datetime(1987, 5, 30).date()))
 connection.commit()
 print("Volumes table: ")
-cursor.execute("SELECT * FROM m_text.Volumes")
+cursor.execute("SELECT * FROM art_info.Volumes")
 for row in cursor:
     print(row)
-cursor.execute("SELECT volume_id FROM m_text.volumes ")
+cursor.execute("SELECT volume_id FROM art_info.volumes ")
 volume_id = cursor.fetchall()[0][0]
 
 # cursor.execute("INSERT INTO Volumes (magazine_id, issue_date) VALUES ('8CD3041608714218BD3850949D53F5E5', "
@@ -66,13 +73,13 @@ cursor.close()
 ##
 cursor = connection.cursor()
 
-cursor.execute("INSERT INTO m_text.Articles (magazine_id, volume_id, article_title, page_range, author_id) " 
-                " VALUES (%s, %s, 'The Big Bang Theory', ARRAY[4,5], 11) ",
+cursor.execute("INSERT INTO art_info.Articles (magazine_id, volume_id, article_title, page_range, author_id) " 
+                " VALUES (%s, %s, 'The Big Bang Theory', ARRAY[4,5], 1) ",
                (magazine_id, volume_id))
 connection.commit()
-cursor.execute("SELECT * FROM m_text.Articles ORDER BY magazine_id, volume_id, article_id ")
+cursor.execute("SELECT * FROM art_info.Articles ORDER BY magazine_id, volume_id, article_id ")
 
-cursor.execute("SELECT volume_id FROM m_text.articles ")
+cursor.execute("SELECT volume_id FROM art_info.articles ")
 article_id = cursor.fetchall()[0][0]
 
 print("Articles table: ")
@@ -88,11 +95,11 @@ cursor.close()
 ##
 cursor = connection.cursor()
 
-cursor.execute(" INSERT INTO m_text.words (word, occurence) VALUES "
-               " ( 'Dog', ARRAY[(%s, %s, %s, ARRAY[(1,2),(2,3),(3,4)]::position_type[])]::occurence_type[]); ",
+cursor.execute(" INSERT INTO text_handle.words (word, occurrence) VALUES "
+               " ( 'Dog', ARRAY[(%s, %s, %s, ARRAY[(1,2),(2,3),(3,4)]::position_type[])]::occurrence_type[]); ",
                (magazine_id, volume_id, article_id))
 connection.commit()
-cursor.execute("SELECT * FROM m_text.words ")
+cursor.execute("SELECT * FROM text_handle.words ")
 
 print("Words table: ")
 
@@ -112,37 +119,63 @@ cursor.close()
 ##
 cursor = connection.cursor()
 
-cursor.execute("INSERT INTO m_text.authors(first_name, last_name, date_of_birth, country) " 
+cursor.execute("INSERT INTO art_info.authors(first_name, last_name, date_of_birth, country) " 
                 " VALUES ( 'Yuval', 'Levy', DATE '1987-05-30', 'Panama') ")
-cursor.execute(" SELECT * FROM m_text.authors ")
+connection.commit()
+cursor.execute(" SELECT * FROM art_info.authors ")
 for row in cursor:
     print(row)
+
+
+## Tests on SearchWizard
+article_id_test = sw.search_author_articles('Yuval Levy')
+print("article id of Yuval Levy is: ")
+print(article_id_test)
+
+article_id_test = sw.search_magazine_articles('National Geographic')
+print("article id of National Geographic is: ")
+print(article_id_test)
+
+article_id_test = sw.search_magazine_volume_articles('National Geographic', 1)
+print("article ids of National Geographic volume 1 is: ")
+print(article_id_test)
+
+article_id_test = sw.search_magazine_volume_page_articles('National Geographic', 1, [4, 5])
+print("article ids of National Geographic volume 1 pages 4-5 is: ")
+print(article_id_test)
+
+article_id_test = sw.search_articles_date(datetime(1987, 5, 30).date())
+print("article ids of 1987-05-30 is: ")
+print(article_id_test)
+## End of tests on SearchWizard
+
+
 
 cursor.close()
 ##
 ## End of tests on Authors table
-
 cursor = connection.cursor()
 
-cursor.execute(" TRUNCATE TABLE m_text.articles ")
-cursor.execute(" DROP TABLE m_text.articles")
+cursor.execute(" DROP TABLE art_info.Articles")
 connection.commit()
-cursor.execute(" TRUNCATE TABLE m_text.volumes")
-cursor.execute(" DROP TABLE m_text.volumes")
+cursor.execute(" TRUNCATE TABLE art_info.volumes")
+cursor.execute(" DROP TABLE art_info.volumes")
 connection.commit()
-cursor.execute(" TRUNCATE TABLE m_text.magazines")
-cursor.execute(" DROP TABLE m_text.magazines")
+cursor.execute(" TRUNCATE TABLE art_info.magazines")
+cursor.execute(" DROP TABLE art_info.magazines")
 connection.commit()
-cursor.execute(" TRUNCATE TABLE m_text.authors")
-cursor.execute(" DROP TABLE m_text.authors")
+cursor.execute(" TRUNCATE TABLE art_info.authors")
+cursor.execute(" DROP TABLE art_info.authors")
 connection.commit()
-cursor.execute(" TRUNCATE TABLE user_input.special_words")
-cursor.execute(" DROP TABLE user_input.special_words")
+cursor.execute(" TRUNCATE TABLE text_handle.special_words")
+cursor.execute(" DROP TABLE text_handle.special_words")
 connection.commit()
-cursor.execute(" DROP TABLE m_text.words")
+cursor.execute(" DROP TABLE text_handle.words")
 connection.commit()
-cursor.execute(" DROP TYPE m_text.occurence_type;")
-cursor.execute(" drop type m_text.position_type;")
+cursor.execute(" DROP TABLE text_handle.phrases")
+connection.commit()
+cursor.execute(" DROP TYPE text_handle.occurrence_type")
+cursor.execute(" DROP TYPE text_handle.position_type")
 connection.commit()
 
 cursor.close()
