6c08ea249caa3a6ebb9c3f6645f2e962aa91deef
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-06-08 22:46:14 +0300

    Update db_handler.py

--- db_handler.py
+++ db_handler.py
@@ -29,8 +29,8 @@ class DB_handler:
         self.connection.commit()
 
     def create_types(self):
-        self.cursor.execute(" CREATE TYPE text_handle.position_type as ( line_number INTEGER, "
-                            "position_in_line INTEGER); ")
+        self.cursor.execute(" CREATE TYPE text_handle.position_type as ( paragraph_number INTEGER, " 
+                            " line_number INTEGER, position_in_line INTEGER); ")
         self.connection.commit()
         self.cursor.execute(" CREATE TYPE text_handle.occurrence_type AS ( magazine_id UUID, volume_id INTEGER, "
                             " article_id INTEGER, positions_array position_type[]); ")
@@ -42,8 +42,7 @@ class DB_handler:
     # Magazines, Volumes, Articles, Authors,
     # As part of the creation of the articles table, a new type is created to store the article's pages
     def create_tables(self):
-        self.cursor.execute(" CREATE TABLE art_info.Magazines(magazine_id UUID PRIMARY KEY, "
-                            " magazine_name TEXT, e_date DATE, location TEXT); ")
+        self.cursor.execute(" CREATE TABLE art_info.Magazines(magazine_id UUID PRIMARY KEY, magazine_name TEXT) ")
         self.connection.commit()
         self.cursor.execute(" CREATE TABLE art_info.Volumes(magazine_id UUID, volume_id INT, issue_date DATE, "
                             " CONSTRAINT pk_volumes PRIMARY KEY (magazine_id, volume_id), "
@@ -61,7 +60,7 @@ class DB_handler:
             " date_of_birth DATE, country TEXT) ")
         self.connection.commit()
         self.cursor.execute(" CREATE TABLE text_handle.words ( word_id SERIAL PRIMARY KEY, "
-                            " word TEXT, occurrence occurrence_type[]); ")
+                            " word TEXT, occurrences occurrence_type[]); ")
         self.connection.commit()
         self.cursor.execute(" CREATE TABLE text_handle.special_words(group_id SERIAL PRIMARY KEY, "
                             " group_description TEXT, words TEXT[])")
@@ -71,22 +70,22 @@ class DB_handler:
 
     def create_triggers(self):
         # Create a trigger that will automatically increment the volume_id when a new volume is added.
-        self.cursor.execute(" CREATE OR REPLACE FUNCTION art_info.generate_volume_id() "
-                            " RETURNS TRIGGER AS $$ "
-                            " DECLARE "
-                            " v_count INTEGER; "
-                            " BEGIN "
-                            " SELECT COUNT(*) INTO v_count FROM art_info.Volumes "
-                            " WHERE magazine_id = NEW.magazine_id; "
-                            " NEW.volume_id := v_count + 1; "
-                            " RETURN NEW; "
-                            " END; "
-                            " $$ LANGUAGE plpgsql; "
-                            " CREATE TRIGGER trigger_generate_volume_id "
-                            " BEFORE INSERT ON art_info.Volumes "
-                            " FOR EACH ROW "
-                            " EXECUTE FUNCTION art_info.generate_volume_id();")
-        self.connection.commit()
+        # self.cursor.execute(" CREATE OR REPLACE FUNCTION art_info.generate_volume_id() "
+        #                     " RETURNS TRIGGER AS $$ "
+        #                     " DECLARE "
+        #                     " v_count INTEGER; "
+        #                     " BEGIN "
+        #                     " SELECT COUNT(*) INTO v_count FROM art_info.Volumes "
+        #                     " WHERE magazine_id = NEW.magazine_id; "
+        #                     " NEW.volume_id := v_count + 1; "
+        #                     " RETURN NEW; "
+        #                     " END; "
+        #                     " $$ LANGUAGE plpgsql; "
+        #                     " CREATE TRIGGER trigger_generate_volume_id "
+        #                     " BEFORE INSERT ON art_info.Volumes "
+        #                     " FOR EACH ROW "
+        #                     " EXECUTE FUNCTION art_info.generate_volume_id();")
+        # self.connection.commit()
         # Create a trigger that will automatically increment the article_id when a new article is added.
         self.cursor.execute(" CREATE OR REPLACE FUNCTION art_info.generate_article_id() "
                             " RETURNS TRIGGER AS $$ "
@@ -111,8 +110,6 @@ class DB_handler:
     # Get author_id from author's name:
     def get_author_id_from_name(self, author_full_name):
         first_name, last_name = parse_name(author_full_name)
-        print("first name: ", first_name)
-        print("last name: ", last_name)
         self.cursor.execute(" SELECT author_id "
                             " FROM art_info.authors "
                             " WHERE first_name = %s AND last_name = %s ",
@@ -126,4 +123,4 @@ class DB_handler:
                             " FROM art_info.magazines "
                             " WHERE magazine_name = %s ",
                             (magazine_name,))
-        return self.cursor.fetchall()[0]
+        return self.cursor.fetchall()
