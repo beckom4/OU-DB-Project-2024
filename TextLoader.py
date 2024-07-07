8eaba40348c7b1f0c869e1c7837f0ba8813041b3
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-07-07 11:07:47 +0300

    Update TextLoader.py

--- TextLoader.py
+++ TextLoader.py
@@ -20,6 +20,12 @@ def parse_name(full_name):
 
     return first_name, last_name
 
+def convert_dicts_to_tuples(array_of_dicts):
+    res = []
+    for d in array_of_dicts:
+        for key, value in d.items():
+            res.append((key, value))
+    return res
 
 class TextLoader:
     def __init__(self):
@@ -40,69 +46,56 @@ class TextLoader:
             ret = author_id[0][0]
         return ret
 
-    def load_magazine(self, magazine_name):
-        magazine_id = self.db_handler.get_magazine_id_from_name(magazine_name)
+    def load_newspaper(self, np_name):
+        np_id = self.db_handler.get_np_id_from_name(np_name)
         # If the magazine is not in the database, we add it.
-        if len(magazine_id) == 0:
-            self.db_handler.cursor.execute(" INSERT INTO art_info.magazines (magazine_id, magazine_name) "
-                                           " VALUES (gen_random_uuid (), %s) RETURNING magazine_id", (magazine_name,))
+        if len(np_id) == 0:
+            self.db_handler.cursor.execute(" INSERT INTO art_info.newspapers (np_id, np_name) "
+                                           " VALUES (gen_random_uuid (), %s) RETURNING np_id", (np_name,))
             self.db_handler.connection.commit()
             ret = self.db_handler.cursor.fetchall()[0][0]
         else:
-            ret = magazine_id[0][0]
+            ret = np_id[0][0]
         return ret
 
-    def load_volume(self, magazine_id, volume_id, issue_date):
-        self.db_handler.cursor.execute("SELECT magazine_id, volume_id "
-                                       " FROM art_info.volumes "
-                                       " WHERE magazine_id = %s AND volume_id = %s ", (magazine_id, volume_id))
-        self.db_handler.connection.commit()
-        volume_id_tuple = self.db_handler.cursor.fetchall()
-        # If the volume is not in the database, we add it.
-        if len(volume_id_tuple) == 0:
-            self.db_handler.cursor.execute(" INSERT INTO art_info.volumes (magazine_id, volume_id, issue_date) "
-                                           " VALUES (%s, %s, %s) RETURNING volume_id ",
-                                           (magazine_id, volume_id, issue_date))
-            self.db_handler.connection.commit()
-            ret = self.db_handler.cursor.fetchall()[0][0]
-        # If the volume is in the database, we return the volume_id.
-        else:
-            ret = volume_id
-        return ret
-
-    def load_article(self, magazine_id, volume_id, article_title, page_range, author_id):
-        self.db_handler.cursor.execute(" INSERT INTO art_info.articles (magazine_id, volume_id, "
-                                       " article_title, page_range, author_id) "
-                                       " VALUES (%s, %s, %s, %s, %s) "
+    def load_article(self, np_id, article_title, date, author_id):
+        self.db_handler.cursor.execute(" INSERT INTO art_info.articles (np_id, article_title, date, author_id) "
+                                       " VALUES (%s, %s, %s, %s) "
                                        " RETURNING article_id",
-                                       (magazine_id, volume_id, article_title, page_range, author_id))
+                                       (np_id, article_title, date, author_id))
         self.db_handler.connection.commit()
-        article_id_tuple = self.db_handler.cursor.fetchall()
-        return article_id_tuple
-
-    def load_text(self, article_id_tuple, word_list):
-        print("word list: ", word_list)
-        for word_occurrence in word_list:
-            word = word_occurrence[0]
-            occurrences = word_occurrence[1]
-            print("word: ", word)
-            print("occurrences: ", occurrences)
-            self.db_handler.cursor.execute("SELECT word_id FROM text_handle.words WHERE word = %s", (word,))
+        article_id = self.db_handler.cursor.fetchall()
+        return article_id
+    #
+    def load_text(self, article_id, disct_text):
+        dis_text = convert_dicts_to_tuples(disct_text)
+        print("complete list: ", dis_text)
+        for word_occurrences in dis_text:
+            self.db_handler.cursor.execute("SELECT word_id FROM text_handle.words WHERE word = %s", (word_occurrences[0],))
             self.db_handler.connection.commit()
             word_id = self.db_handler.cursor.fetchall()
             # If the word is not in the database, we add it.
             if len(word_id) == 0:
-                self.db_handler.cursor.execute("INSERT INTO text_handle.words (word, occurrences) VALUES "
-                                               "  (%s, ARRAY[(%s,%s,%s,%s::position_type[])]::occurrence_type[]) ",
-                                               (word, article_id_tuple[0], article_id_tuple[1],
-                                                article_id_tuple[2], occurrences))
+                self.db_handler.cursor.execute(" INSERT INTO text_handle.words (word, occurrences) VALUES ( "
+                                               " %s,  ARRAY[ "
+                                               "ROW( %s, %s::position_type[])]::occurrence_type[]) ",
+                                               (word_occurrences[0], article_id[0][0], word_occurrences[1]))
                 self.db_handler.connection.commit()
             # If the word is in the database, we add the new occurrences.
             else:
-                self.db_handler.cursor.execute("UPDATE text_handle.words "
-                                               " SET occurrences = ARRAY_APPEND(occurrences, "
-                                               "(%s,%s,%s,%s::position_type[])::occurrence_type) "
-                                               " WHERE word_id = %s ",
-                                               (article_id_tuple[0], article_id_tuple[1],
-                                                article_id_tuple[2], occurrences, word_id[0][0]))
+                new_positions_array = "ARRAY[%s]::position_type[]" % ','.join(
+                    "ROW(%s, %s, %s, '%s', '%s', '%s')" % pos for pos in word_occurrences[1])
+
+                # Create the new occurrence record
+                new_occurrence_record = "ROW(%s, %s)::occurrence_type" % (article_id[0][0], new_positions_array)
+
+                # Update the table to add the new occurrence
+                self.db_handler.cursor.execute(
+                    f"""
+                    UPDATE text_handle.words 
+                    SET occurrences = array_append(occurrences, {new_occurrence_record})
+                    WHERE word = %s
+                    """,
+                    (word_occurrences[0],)
+                )
                 self.db_handler.connection.commit()
