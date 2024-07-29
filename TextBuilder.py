16e08898fa99411021c2537cd0d6c6e22d9942d9
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-07-29 20:51:38 +0300

    Update TextBuilder.py

--- TextBuilder.py
+++ TextBuilder.py
@@ -3,18 +3,32 @@ from LinkedList import *
 import re
 import ast
 
-
-
+def parse_string_to_tuple_con(word, input_string):
+    res = []
+    tuple_strings = (input_string
+                                    .replace(r'"', '') \
+                                    .replace(r'\\"" \\""', "' '")\
+                                    .replace('\n', '@@@') \
+                                    .replace("\\", "'"))
+    parsed_tuple = ast.literal_eval(tuple_strings.replace('{', '[').replace('}', ']'))
+    array_of_tuples = list(parsed_tuple)
+    for tup in array_of_tuples:
+        temp_str = tup[3].replace(r'@@@@@@', '\n\n')\
+                         .replace(r'@@@', '\n')\
+                         .replace(r'&&&', '"')
+        final_word = word + temp_str
+        new_tup = tup[:3] + (final_word,)
+        res.append(new_tup)
+    return res
 def parse_string_to_tuple(word, input_string):
     res = []
-    # print("input string is: ", input_string)
     tuple_strings = (input_string
                                     .replace(r'"', '') \
                                     .replace(r'\\"" \\""', "' '")\
                                     .replace(r'\\', "'")\
                                     .replace('\n', '@@@') \
                                     .replace(r'\\, \\', ','))
-    parsed_tuple = ast.literal_eval(tuple_strings)
+    parsed_tuple = ast.literal_eval(tuple_strings.strip('{}'))
     array_of_tuples = list(parsed_tuple[1])
     for tup in array_of_tuples:
         temp_str = tup[3].replace(r'@@@@@@', '\n\n')\
@@ -34,7 +48,6 @@ class TextBuilder:
     def build_entire_text(self, article_title):
         text_arr = []
         article_id = self.db_handler.get_article_id_from_title(article_title)[0][0]
-        print("article_id: ", article_id)
         self.db_handler.cursor.execute("""SELECT a.article_title, a.date, r.first_name, r.last_name 
                                           FROM art_info.articles a JOIN art_info.reporters r 
                                           ON a.reporter_id = r.reporter_id 
@@ -42,29 +55,19 @@ class TextBuilder:
                                          (article_id, ))
         self.db_handler.connection.commit()
         article_title, date_of_issue, rep_f_name, rep_last_name = self.db_handler.cursor.fetchall()[0]
-        print("article_title: ", article_title)
-        print("date_of_issue: ", date_of_issue)
-        print("rep_f_name: ", rep_f_name)
-        print("rep_last_name: ", rep_last_name)
         self.db_handler.cursor.execute( " SELECT word_id, word, unnest(occurrences) "
                                         " FROM text_handle.words, "
                                         " LATERAL unnest(occurrences) AS outer_tuple "
                                         " WHERE (outer_tuple).article_id = %s ",
                                         (article_id, ))
         self.db_handler.connection.commit()
-        print("Test Words table: ")
         for row in self.db_handler.cursor:
-            # print("each row is :")
-            # print(row)
             tup_arr = parse_string_to_tuple(row[1],row[2])
             text_arr.extend(tup_arr)
         sorted_text_arr = sorted(text_arr, key=lambda x: (x[0], x[1], x[2]))
-        print("sorted_text_arr: ", sorted_text_arr)
         final_text = ""
         for tup in sorted_text_arr:
             final_text += f"{tup[3]}"
-        print("final_text: ")
-        print(final_text)
 
     def all_words(self):
         self.db_handler.cursor.execute(" SELECT word "
@@ -78,6 +81,64 @@ class TextBuilder:
                                        " WHERE (occ).article_id = %s", (article_id,))
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
+
+    def build_context(self, article_title, word):
+        res =[]
+        lines_arr = []
+        article_id = self.db_handler.get_article_id_from_title(article_title)[0][0]
+        word_id = self.db_handler.get_word_id_from_word(word)[0][0]
+        self.db_handler.cursor.execute(" SELECT word, unnest(occurrences) "
+                                       " FROM text_handle.words, "
+                                       " LATERAL unnest(occurrences) AS outer_tuple "
+                                       " WHERE (outer_tuple).article_id = %s and word_id = %s",
+                                       (article_id,word_id))
+        self.db_handler.connection.commit()
+
+        # Build the text array with tuples
+        for row in self.db_handler.cursor:
+            tup_arr = parse_string_to_tuple(row[0], row[1])
+        for tup in tup_arr:
+            line = (tup[0], tup[1])
+            lines_arr.append(line)
+        for line in lines_arr:
+            query = """
+                SELECT word_id, word, 
+                       ARRAY(
+                           SELECT pos
+                           FROM unnest((outer_tuple).positions) AS pos
+                           WHERE pos.paragraph_number = %s
+                           AND pos.line_number IN (%s, %s, %s)
+                       ) AS filtered_positions
+                FROM text_handle.words,
+                     LATERAL unnest(occurrences) AS outer_tuple
+                WHERE (outer_tuple).article_id = %s
+                  AND EXISTS (
+                      SELECT 1
+                      FROM unnest((outer_tuple).positions) AS pos
+                      WHERE pos.paragraph_number = %s
+                      AND pos.line_number IN (%s, %s, %s)
+                  )
+            """
+            self.db_handler.cursor.execute(query, (line[0],line[1]-1,line[1],line[1]+1,
+                                                   article_id,line[0],line[1]-1,line[1],line[1]+1))
+            self.db_handler.connection.commit()
+            result = self.db_handler.cursor.fetchall()
+            text_arr = []
+            for tup in result:
+                tup_arr = parse_string_to_tuple_con(tup[1], tup[2])
+                text_arr.extend(tup_arr)
+            sorted_text_arr = sorted(text_arr, key=lambda x: (x[0], x[1], x[2]))
+            final_context = ""
+            for tup in sorted_text_arr:
+                final_context += f"{tup[3]}"
+            res.append(final_context)
+        return res
+
+
+
+
+
+
 
 
 
