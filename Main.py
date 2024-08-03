d97423ed12b4c13d0948d306555964eb631c2095
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-08-03 18:24:50 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,23 @@
-from Database import Database
+from DB_handler import *
 from StreamlitUI import StreamlitUI
 
 class Main:
     def __init__(self):
-        self.database = Database()
+        self.database = DB_handler()
         self.ui = StreamlitUI(self.database)
 
+    def init_db(self):
+        self.database.create_schemas()
+        self.database.create_types()
+        self.database.create_tables()
+
     def run(self):
+        # db_test.create_triggers()
         self.ui.run()
 
 
 
 if __name__ == "__main__":
     app = Main()
+    app.init_db()
     app.run()
