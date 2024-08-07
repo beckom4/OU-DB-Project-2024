a0b1a067fb666c8fe97be1af30c77bc90ca9f424
Omri Beck <126347234+beckom4@users.noreply.github.com>
2024-08-07 23:16:49 +0300

    Update TextBuilder.py

--- TextBuilder.py
+++ TextBuilder.py
@@ -2,12 +2,37 @@ from DB_handler import DB_handler
 import re
 import ast
 
+def parse_array_of_tuples(array_string):
+    # Remove the curly braces
+    array_string = array_string.strip('{}')
+    # Regular expression to match tuples
+    tuple_pattern = re.compile(r'\(([^)]+)\)')
+    # Find all tuple strings
+    tuples = tuple_pattern.findall(array_string)
+    parsed_tuples = []
+    for tup in tuples:
+        parsed_tuples.append(parse_tuple_string(tup))
+    return parsed_tuples
+
+
+def parse_tuple_string(tuple_string):
+    # Remove the parentheses
+    tuple_string = tuple_string.strip('()')
+
+    # Split the string by commas
+    elements = re.split(r',\s*', tuple_string)
+
+    first_three_integers = [int(elements[i].strip()) for i in range(3)]
+    last_two_strings = [elements[i].strip() for i in range(3, 5)]
+
+    return tuple(first_three_integers + last_two_strings)
+
 def parse_string_to_tuple_ind(input_string):
     res = []
     tuple_strings = (input_string
                                     .replace(r'"', '') \
                                     .replace("\\",''))
-    tup_arr = ast.literal_eval(tuple_strings.replace('{{', '[').replace('}}', ']'))
+    tup_arr = ast.literal_eval(tuple_strings.replace('{', '[').replace('}', ']'))
     return tup_arr
 
 
@@ -31,18 +56,27 @@ def parse_string_to_tuple_con(word, input_string):
 def parse_string_to_tuple(word, input_string):
     res = []
     tuple_strings = (input_string
+                                    .replace(r'\"', '') \
                                     .replace(r'"', '') \
-                                    .replace(r'\\"" \\""', "' '")\
-                                    .replace(r'\\', "'")\
+                                    .replace("\\", '')\
                                     .replace('\n', '@@@') \
+                                    # .replace('&&&', "'&&&'") \
                                     .replace(r'\\, \\', ','))
-    parsed_tuple = ast.literal_eval(tuple_strings.strip('{}'))
-    array_of_tuples = list(parsed_tuple[1])
-    for tup in array_of_tuples:
-        temp_str = tup[3].replace(r'@@@@@@', '\n\n')\
-                         .replace(r'@@@', '\n')\
-                         .replace(r'&&&', '"')
-        final_word = word + temp_str
+    actual_array = parse_array_of_tuples(tuple_strings)
+    for tup in actual_array:
+        temp_str_beg = tup[4].replace(r"&&&", '"') \
+                             .replace(r'hhhaaa', '(') \
+                             .replace(r'pppuuu&&&', ',"') \
+                             .replace(r'&&&pppuuu', '",')
+        temp_str_end = tup[3].replace(r'@@@@@@', '\n\n')\
+                             .replace(r'@@@', '\n')\
+                             .replace(r'pppuuu&&&', ',"')\
+                             .replace(r'&&&pppuuu', '",')\
+                             .replace(r'&&&', '"') \
+                             .replace(r'pppuuu', ',') \
+                             .replace(r'hhhaaa', '(') \
+                             .replace(r'hhhbbb', ')')
+        final_word = temp_str_beg + word + temp_str_end
         new_tup = tup[:3] + (final_word,)
         res.append(new_tup)
     return res
@@ -53,7 +87,11 @@ class TextBuilder:
 
     def build_entire_text(self, article_title):
         text_arr = []
-        article_id = self.db_handler.get_article_id_from_title(article_title)[0][0]
+        art_id_full = self.db_handler.get_article_id_from_title(article_title)
+        if len(art_id_full) == 0:
+            return None
+        else:
+            article_id = art_id_full[0][0]
         self.db_handler.cursor.execute("""SELECT a.article_title, a.date, r.first_name, r.last_name 
                                           FROM art_info.articles a JOIN art_info.reporters r 
                                           ON a.reporter_id = r.reporter_id 
@@ -61,19 +99,27 @@ class TextBuilder:
                                          (article_id, ))
         self.db_handler.connection.commit()
         article_title, date_of_issue, rep_f_name, rep_last_name = self.db_handler.cursor.fetchall()[0]
-        self.db_handler.cursor.execute( " SELECT word_id, word, unnest(occurrences) "
-                                        " FROM text_handle.words, "
-                                        " LATERAL unnest(occurrences) AS outer_tuple "
-                                        " WHERE (outer_tuple).article_id = %s ",
+        self.db_handler.cursor.execute( """ with position_aggregation as (
+                                            SELECT word_id, word, outer_tuple
+                                            FROM text_handle.words, 
+                                            LATERAL unnest(occurrences) AS outer_tuple 
+                                            WHERE (outer_tuple).article_id = %s )
+                                            SELECT word_id, word, (outer_tuple).positions 
+                                            FROM position_aggregation """,
                                         (article_id, ))
         self.db_handler.connection.commit()
+        rep_full_name = rep_f_name + " " + rep_last_name
         for row in self.db_handler.cursor:
             tup_arr = parse_string_to_tuple(row[1],row[2])
             text_arr.extend(tup_arr)
         sorted_text_arr = sorted(text_arr, key=lambda x: (x[0], x[1], x[2]))
         final_text = ""
         for tup in sorted_text_arr:
-            final_text += f"{tup[3]}"
+            if tup[2] == 1:
+                final_text += f"{tup[3]}"
+            else:
+                final_text += " " + f" {tup[3]}"
+        return article_title, date_of_issue, rep_full_name, final_text
 
     def all_words(self):
         self.db_handler.cursor.execute(" SELECT word "
@@ -81,10 +127,12 @@ class TextBuilder:
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
 
-    def all_words_in_article(self, article_id):
+    def all_words_in_article(self, article_title):
+        article_id = self.db_handler.get_article_id_from_title(article_title)[0][0]
         self.db_handler.cursor.execute(" SELECT word "
                                        " from text_handle.words, unnest(occurrences) AS occ "
-                                       " WHERE (occ).article_id = %s", (article_id,))
+                                       " WHERE (occ).article_id = %s"
+                                       " order by occ ", (article_id,))
         self.db_handler.connection.commit()
         return self.db_handler.cursor.fetchall()
 
@@ -144,25 +192,23 @@ class TextBuilder:
     # Search for all the articles that contain a specific word.
     # An index is defined as the position of the word in the article.
     # The position consists of the paragraph number, the line number and the position in the line.
-    def build_words_index(self, word, article_title):
-        word_id = self.db_handler.get_word_id_from_word(word)
-        article_id = self.db_handler.get_article_id_from_title(article_title)[0][0]
-        query = """
-            WITH position_aggregation AS (
-                SELECT word,
-                       occ.article_id,
-                       ARRAY_AGG((pos.paragraph_number, pos.line_number, pos.position_in_line)) AS positions
-                FROM words,
-                     LATERAL unnest(occurrences) AS occ(article_id, positions),
-                     LATERAL unnest(occ.positions) AS pos(paragraph_number, line_number, position_in_line, finishing_chars)
-                GROUP BY word, occ.article_id
-            )
-            SELECT word,
-                   ARRAY_AGG( positions) AS filtered_occurrences
-            FROM position_aggregation
-            GROUP BY word;
-        """
-        self.db_handler.cursor.execute(query)
+    def build_words_index(self, article_title):
+        art_id_full = self.db_handler.get_article_id_from_title(article_title)
+        if len(art_id_full) == 0:
+            return None
+        else:
+            article_id = art_id_full[0][0]
+        self.db_handler.cursor.execute( """ SELECT word,
+                                            ARRAY_AGG((pos.paragraph_number, pos.line_number, 
+                                            pos.position_in_line)) AS positions
+                                            FROM text_handle.words,
+                                            LATERAL unnest(occurrences) AS occ(article_id, positions),
+                                            LATERAL unnest(occ.positions) AS pos(paragraph_number, line_number, 
+                                            position_in_line, finishing_chars, starting_chars)
+                                            WHERE occ.article_id = %s
+                                            GROUP BY word
+                                            ORDER BY word """,
+                                        (article_id, ))
         self.db_handler.connection.commit()
         res = []
         for row in self.db_handler.cursor:
