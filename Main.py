0dec21ce5d9f1e4aaf79ba1af125d75adcdd2935
Eran Holzman <133662392+Eran-Holzman@users.noreply.github.com>
2024-08-17 22:53:57 +0300

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,18 +1,46 @@
-from DB_handler import *
-from StreamlitUI import StreamlitUI
+from db_handler import *
+from streamlitUI import StreamlitUI
+
 
 class Main:
+    """
+     Main class that orchestrates the application's components.
+
+     This class initializes the database handler and the StreamlitUI,
+     and provides methods to set up the database and run the application.
+
+     Attributes:
+         database (DB_handler): An instance of the database handler.
+         ui (StreamlitUI): An instance of the Streamlit user interface.
+     """
     def __init__(self):
+        """
+           Initialize the Main class.
+
+           Creates instances of the DB_handler and StreamlitUI classes.
+           """
         self.database = DB_handler()
         self.ui = StreamlitUI(self.database)
 
     def init_db(self):
+        """
+        Initialize the database.
+
+        This method sets up the database by creating necessary schemas,
+        custom types, tables, triggers, and views.
+        """
         self.database.create_schemas()
         self.database.create_types()
         self.database.create_tables()
+        self.database.create_triggers()
+        self.database.create_view()
 
     def run(self):
-        # db_test.create_triggers()
+        """
+        Run the application.
+
+        This method starts the Streamlit user interface.
+        """
         self.ui.run()
 
 
